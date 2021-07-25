379fede23d9b5f2a0244e43149adee509bc3af23
sachin2323 <sachinsingh328@gmail.com>
2021-07-25 15:19:24 +0530

    learnt forms, swiggy

--- node-express-server/src/index.js
+++ node-express-server/src/index.js
@@ -3,11 +3,27 @@ import cors from 'cors';
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/forms', (req, res) => {
+  console.log(req.query);
+  res.end('Received');
+});
+
+app.post('/forms', (req, res) => {
+  console.log(req.body);
+  res.end('Received');
 });
 
 app.listen(process.env.PORT, () =>
