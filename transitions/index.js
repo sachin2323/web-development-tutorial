c38d8b37a192342710a16a79030235d12ed7cc60
sachin2323 <sachinsingh328@gmail.com>
2021-08-01 13:04:29 +0530

    transitions

--- transitions/index.js
+++ transitions/index.js
@@ -1,5 +1,7 @@
 var element = document.getElementsByClassName("ball");
+console.log("element", element);
 window.addEventListener("click", function (event) {
+  console.log("event", event);
   const { clientX, clientY } = event;
   element[0].style.transform = `translate(${clientX - 50}px,${clientY - 50}px)`;
 });
