dcb2d72ebc63a7dc6851f2bc6dc78fa07a689588
sachin2323 <sachinsingh328@gmail.com>
2021-08-07 14:45:57 +0530

    update

---
+++ sandbox.js
@@ -0,0 +1,124 @@
+// let movie = "Spidermannnn";
+
+// console.log("length of movie", movie.length);
+
+// console.log("fetch S of movie", movie[8]);
+
+// console.log(movie);
+
+// const db = {
+//   firstName: "Sachin",
+//   lastName: "Singh",
+// };
+// // concatenate
+
+// second_movie = movie + " " + +"2";
+
+// console.log(second_movie);
+
+// let doctor_name = "Dr." + db.firstName + " " + db["lastName"];
+
+// console.log(doctor_name);
+
+// // string Methods
+
+// console.log(doctor_name.toLowerCase());
+// console.log(second_movie.indexOf("2"));
+// console.log(second_movie.replace("man", "bot"));
+// console.log(second_movie.lastIndexOf("s"));
+// console.log(second_movie.slice(4, 6));
+// console.log(second_movie.substr(4, 6));
+
+// Numbers
+
+// //  Operators -> +, -, *, /, %, **, ++, --
+// let a = "abcde";
+// let b = 1;
+
+// // let c = a % b;
+// // console.log(c);
+// // c++;
+// // c--;
+
+// // c = c - 2;
+
+// // c **= 3; // c = c + 2
+// // console.log(c);
+
+// // NaN -> not a number
+
+// c = a * b; // c = c + 2
+// console.log(c);
+
+// Template Strings
+
+const title = " Best books of 2021";
+const author = "Adarsh";
+let likes = 30;
+
+// simple concatenation
+// let str =
+//   "the blog called " +
+//   title +
+//   "written by " +
+//   author +
+//   "has " +
+//   likes +
+//   "likes";
+
+// template strings
+
+// let str = `the blog called ${title} written by ${author.toUpperCase()} has ${(likes += 2)} likes`;
+// let name = "sachin";
+// let product = "Mac M1 pro";
+// const mail = `<html>
+// <body>
+// This mail is intended to ${name};
+// Thank you for purchasing ${product}
+// </body>
+// </html>
+// `;
+// console.log(mail);
+
+// Arrays
+
+// let fruits = ["orange", "apple", "mango", "banana"];
+
+// let basket = ["green", "blue", "yellow"];
+
+// let new_array = [...fruits, 1, 2, 3, 4, ...basket];
+
+// console.log(new_array);
+// console.log(fruits);
+// // fruits[1] = "chikoo";
+// // console.log(fruits);
+
+// // console.log(fruits[1]);
+
+// // fruits = fruits.concat(["chikoo", "mango"]);
+
+// // fruits.push("mango");
+
+// // console.log(fruits.pop());
+
+// console.log(fruits.slice(1, 3));
+
+// undefined and null
+
+// let a;
+// let b = "null";
+
+// console.log(typeof a, typeof b);
+
+// comparision opertors ==, ===, !=, >, <
+
+// let a = 5;
+// let b = Number("3.14");
+
+// console.log(typeof a, typeof b);
+
+// if (a === b) {
+//   console.log(b.toLowerCase());
+// }
+
+// console.log(a === b);
